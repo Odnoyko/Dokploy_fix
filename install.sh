b72083a6111a0b32c24e51a17f42fd4c21ee0bd5
Vitaly <kontakt@odnoyko.com>
2025-11-13 19:59:31 +0100

    Update Traefik image version to v3.6.1

--- install.sh
+++ install.sh
@@ -189,7 +189,7 @@ install_dokploy() {
         -p 80:80/tcp \
         -p 443:443/tcp \
         -p 443:443/udp \
-        traefik:v3.6.0
+        davidfrickert/traefik:v3.6.1
     
     docker network connect dokploy-network dokploy-traefik
 
